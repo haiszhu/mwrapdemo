77b3da9b652aed149dcf35e8cd1fae80ad7f78cb
Hai Zhu <zhuh1106@gmail.com>
2026-01-13 17:49:47 +0800

    ...

---
+++ c/lib.h
@@ -0,0 +1,8 @@
+#ifndef LIB_H
+#define LIB_H
+
+#include <complex.h>
+
+void myfunc(int N, const double* x, double* y, double complex* w, int flag);
+
+#endif
---
+++ c2domp/lib.h
@@ -0,0 +1,7 @@
+#ifndef LIB_H
+#define LIB_H
+
+void array2d(int n, double* a, int reps);
+void array2domp(int n, double* a, int reps);
+
+#endif
